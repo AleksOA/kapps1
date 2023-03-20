4c888dd810eb5cbaf98d5c51b220e5fc182c39b0
AleksOA <ovcharenko.alekss@gmail.com>
2023-03-20 15:51:50 +0200

    section text

--- wp-content/themes/kapps/template_pages/services_template.php
+++ wp-content/themes/kapps/template_pages/services_template.php
@@ -18,26 +18,26 @@
         <?php  get_template_part( 'let_is_talk_part' );  ?>
     </section>
 
-<!--    <section>-->
-<!--        <div class="services-text">-->
-<!--            <div class="container">-->
-<!--                <div class="services-text__wrapper">-->
-<!--                    <div class="services-text__item-one">-->
-<!--                        --><?php
-//                        $item_one_services_text = get_field('item_one_services_text');
-//                        if( $item_one_services_text ) { echo $item_one_services_text;}
-//                        ?>
-<!--                    </div>-->
-<!--                    <div class="services-text__item-two">-->
-<!--                        --><?php
-//                        $item_two_services_text = get_field('item_two_services_text');
-//                        if( $item_two_services_text ) { echo $item_two_services_text;}
-//                        ?>
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </section>-->
+    <section>
+        <div class="services-text">
+            <div class="container">
+                <div class="services-text__wrapper">
+                    <div class="services-text__item-one">
+                        <?php
+                        $item_one_services_text = get_field('item_one_services_text');
+                        if( $item_one_services_text ) { echo $item_one_services_text;}
+                        ?>
+                    </div>
+                    <div class="services-text__item-two">
+                        <?php
+                        $item_two_services_text = get_field('item_two_services_text');
+                        if( $item_two_services_text ) { echo $item_two_services_text;}
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <section>
         <?php  get_template_part( 'contacts_part' );  ?>
