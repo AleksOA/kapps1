2e89ee7b4e7f8d438f35f37ff0b1312e4d73ea25
AleksOA <ovcharenko.alekss@gmail.com>
2023-03-25 19:48:22 +0200

    finished contact and team pages

--- wp-content/themes/kapps/contact_form_part.php
+++ wp-content/themes/kapps/contact_form_part.php
@@ -1,3 +1,9 @@
 <div class="contacts">
-    <?php echo do_shortcode('[contact-form-7 id="262" title="Contacts"]') ?>
+    <?php
+    if($args){
+        $text_for_form = $args["section_data"];
+        if($text_for_form) {echo '<div class="top"><p>' . $text_for_form . '</p></div>';}
+    }
+    echo do_shortcode('[contact-form-7 id="262" title="Contacts"]')
+    ?>
 </div>
